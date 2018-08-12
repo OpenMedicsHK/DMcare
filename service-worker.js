bab47635de80a5f3f9310543265a6a3223e5cbc1
Matthew <Matthew@Matthews-MacBook-Pro.local>
2018-08-12 21:09:40 +0800

    update

--- service-worker.js
+++ service-worker.js
@@ -17,8 +17,8 @@ const networkFirstStrategy = (name) => workboxSW.strategies.networkFirst(cachePa
 // Placeholder array which is populated automatically by workboxBuild.injectManifest()
 workboxSW.precache([
   {
-    "url": "/index.html",
-    "revision": "a3cb17a06ab1528116370b9d652c08e1"
+    "url": "/DMcare/index.html",
+    "revision": "511c39e153db31fbc2627de52465ba0e"
   },
   {
     "url": "OneSignalSDKUpdaterWorker.js",
@@ -30,70 +30,78 @@ workboxSW.precache([
   },
   {
     "url": "service-worker.js",
-    "revision": "04a1f858bec793c2ed22d1bb9832646f"
+    "revision": "656442bac41ee41bddc67a700ef819eb"
   },
   {
-    "url": "/static/css/app.333f30ebac819f2baaaae5524d8f9b1e.css",
-    "revision": "d06735e04e6f6c0f5adf928e9f50342a"
+    "url": "/DMcare/static/css/app.c68de87fd0b8ab8e574b63238df1fbeb.css",
+    "revision": "c165aaacc8e5402ba267d99eeb427574"
   },
   {
-    "url": "/static/img/icons/android-chrome-192x192.png",
+    "url": "/DMcare/static/img/hypergly.jpg",
+    "revision": "9ab8ae5125b63ad6753e0cf1440ca2e6"
+  },
+  {
+    "url": "/DMcare/static/img/hypogly.jpg",
+    "revision": "221989e82b38077b607299b798473a19"
+  },
+  {
+    "url": "/DMcare/static/img/icons/android-chrome-192x192.png",
     "revision": "fa651aecfc4c1f21fb9c7c0ec60974ed"
   },
   {
-    "url": "/static/img/icons/android-chrome-512x512.png",
+    "url": "/DMcare/static/img/icons/android-chrome-512x512.png",
     "revision": "9b9c6c89c92e5bb841067944de3e466c"
   },
   {
-    "url": "/static/img/icons/apple-touch-icon-120x120.png",
+    "url": "/DMcare/static/img/icons/apple-touch-icon-120x120.png",
     "revision": "936d6e411cabd71f0e627011c3f18fe2"
   },
   {
-    "url": "/static/img/icons/apple-touch-icon-152x152.png",
+    "url": "/DMcare/static/img/icons/apple-touch-icon-152x152.png",
     "revision": "1a034e64d80905128113e5272a5ab95e"
   },
   {
-    "url": "/static/img/icons/apple-touch-icon-180x180.png",
+    "url": "/DMcare/static/img/icons/apple-touch-icon-180x180.png",
     "revision": "c43cd371a49ee4ca17ab3a60e72bdd51"
   },
   {
-    "url": "/static/img/icons/apple-touch-icon-60x60.png",
+    "url": "/DMcare/static/img/icons/apple-touch-icon-60x60.png",
     "revision": "9a2b5c0f19de617685b7b5b42464e7db"
   },
   {
-    "url": "/static/img/icons/apple-touch-icon-76x76.png",
+    "url": "/DMcare/static/img/icons/apple-touch-icon-76x76.png",
     "revision": "af28d69d59284dd202aa55e57227b11b"
   },
   {
-    "url": "/static/img/icons/apple-touch-icon.png",
+    "url": "/DMcare/static/img/icons/apple-touch-icon.png",
     "revision": "e06552396e954fc6ddabea07ba308492"
   },
   {
-    "url": "/static/img/icons/favicon-16x16.png",
+    "url": "/DMcare/static/img/icons/favicon-16x16.png",
     "revision": "ab827c537ba95dc20585b5e8ceacc601"
   },
   {
-    "url": "/static/img/icons/favicon-32x32.png",
+    "url": "/DMcare/static/img/icons/favicon-32x32.png",
     "revision": "499fb32654b66fcaa0b8d9729c1dbbbd"
   },
   {
-    "url": "/static/img/icons/msapplication-icon-144x144.png",
+    "url": "/DMcare/static/img/icons/msapplication-icon-144x144.png",
     "revision": "b89032a4a5a1879f30ba05a13947f26f"
   },
   {
-    "url": "/static/img/icons/mstile-150x150.png",
+    "url": "/DMcare/static/img/icons/mstile-150x150.png",
     "revision": "744b3f2c9bc3f0b7339082d73954177f"
   },
   {
-    "url": "/static/js/app.58c04837fe7b7437fe51.js",
-    "revision": "0f6f659416824554f2bf75e712de6041"
+    "url": "/DMcare/static/js/app.20e8347ef6cad8b154b6.js",
+    "revision": "30ec22af50882c89aada358297f1e772"
   },
   {
-    "url": "/static/js/manifest.9944038d8634c59b2473.js",
-    "revision": "586e3e515f39c559be3e719ba4f302c4"
+    "url": "/DMcare/static/js/manifest.9b375dffa331ffcc72b7.js",
+    "revision": "310d7707518535de129801de7a3147fb"
   },
   {
-    "url": "/static/js/vendor.b705f013b411880df5e8.js",
+    "url": "/DMcare/static/js/vendor.b705f013b411880df5e8.js",
     "revision": "3262d440fb6f4135fc5f5d993b161fc9"
   },
   {
